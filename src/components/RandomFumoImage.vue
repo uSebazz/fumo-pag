d5f78b08813f2249d72e8b3328732ce681235ad7
uSebazz <iSebazz@outlook.es>
2022-07-23 18:54:22 -0500

    fixing fumos images

--- src/components/RandomFumoImage.vue
+++ src/components/RandomFumoImage.vue
@@ -27,7 +27,10 @@ export default {
       const data = await req.json()
       this.image = data.URL;
       this.alt = data._id;
-      this.loaded = true;
+      this.loaded = false
+      setTimeout(() => {
+        this.loaded = true
+      }, 1000);
 
     }
   },
@@ -38,10 +41,15 @@ export default {
 <template>
   <div v-if="loaded">
     <section class="flex h-screen text-white items-center justify-center">
-      <img :src="image" :alt="alt" class="object-cover rounded-lg" />
-      <button @click="anotherFumo" type="button"
+
+      <!-- The Fumo Image-->
+      <img :src="image" :alt="alt" class="object-cover rounded-lg h-auto w-[600px]" />
+
+      <button v-if="loaded" @click="anotherFumo" type="button"
         class="px-4 py-2 m-5  font-sans font-semibold text-sm bg-green-400 text-white rounded-lg shadow-sm">
         Reload </button>
+      <LoadingPage v-else />
+
     </section>
   </div>
   <LoadingPage v-else />
